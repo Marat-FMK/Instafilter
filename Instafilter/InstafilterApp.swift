0787b26ea0e7ef56eabd7789a526e893ae3666cd
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2024-10-29 18:42:32 +0300

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Marat Fakhrizhanov on 29.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
